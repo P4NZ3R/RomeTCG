122e28e4a5f96d9634ac1217864264792414182a
Andrea <dambrosio.andry@gmail.com>
2017-06-13 12:19:36 +0200

    added UnitScript

---
+++ Assets/Script/ScriptUnit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ScriptUnit : ScriptableObject 
+{
+    public string name;
+    public int tier;
+    public int damage;
+    public int armor;
+    public Faction faction;
+    public Type type;
+    public Features[] features;
+    public string description;
+    public Texture img;
+
+    public enum Faction 
+    {
+        roman,averni,suebi,boii
+    }
+
+    public enum Type
+    {
+        infantry
+    }
+
+    public enum Features
+    {
+        pila,gladio,scutum,sword,shield,spear,club,axe
+    }
+}
