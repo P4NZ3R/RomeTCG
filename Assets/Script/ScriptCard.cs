11c28a9d2c4b2944f5793dc839f76e7d4e056170
Andrea <dambrosio.andry@gmail.com>
2017-06-13 14:01:03 +0200

    added few cards

---
+++ Assets/Script/ScriptCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ScriptCards : ScriptableObject {
+
+    public string name;
+    public Rarity rarity;
+    public short cost;
+    public string effect;
+
+    public enum Rarity
+    {
+        basic,common,rare,epic,legendary,special
+    }
+}
